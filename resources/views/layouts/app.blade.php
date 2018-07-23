9422cd692b7f17cb722ed8b7a7c4413c02fe3faf
Terrence Tejada <tejada.terrence@gmail.com>
2018-07-23 20:41:16 +0800

    Add Storage Link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('storage/img/favicon.png') }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
